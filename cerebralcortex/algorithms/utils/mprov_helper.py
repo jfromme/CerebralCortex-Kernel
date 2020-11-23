702e0dea7e6c4ebd2b1ed84c2f119690f0dceb7c
John Frommeyer <johnfr@seas.upenn.edu>
2020-11-23 12:19:42 -0500

    Revert "mprov changes"
    This reverts commit 1c9b1c7bdbac048e72c10995ceb06f4bcb742d94.

--- cerebralcortex/algorithms/utils/mprov_helper.py
+++ cerebralcortex/algorithms/utils/mprov_helper.py
@@ -49,7 +49,6 @@ def MProvAgg_empty():
         return wrapper
     return inner_function
 
-
 def CC_get_prov_connection(graph_name=None):
     mprov = os.getenv("ENABLE_MPROV")
     if graph_name is None:
